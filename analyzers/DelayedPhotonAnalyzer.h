07f2e0475c11dae74e546e8d5753a659c2b528d7
Thong <thongnguyen1292@gmail.com>
2019-12-16 22:57:44 -0800

    Update N_EB and C_EB

--- analyzers/DelayedPhotonAnalyzer.h
+++ analyzers/DelayedPhotonAnalyzer.h
@@ -15,15 +15,15 @@ class DelayedPhotonAnalyzer: public RazorAnalyzer {
         vector <float> *rms_G6_all;
         vector <int> *detID_all;
 	
-	const double N_EB = 33.6;   //ns
-        const double C_EB = 0.083; //ns
-        const double N_EB_MC = 46.3; //ns
-        const double C_EB_MC = 0.091; //ns
+        const double N_EB = 30.4;   //ns
+        const double C_EB = 0.095; //ns
+        const double N_EB_MC = 34.3; //ns
+        const double C_EB_MC = 0.103; //ns
 
         DelayedPhotonAnalyzer(TTree *tree=0): RazorAnalyzer(tree) { }
         void Analyze(bool isData, int option, string outputFileName, string label);
-	float getADCToGeV( uint run, int isEBOrEE);
- 	TVector3 intersectPoint (float x0,float y0,float z0,float px,float py,float pz,float R);
+        float getADCToGeV( uint run, int isEBOrEE);
+        TVector3 intersectPoint (float x0,float y0,float z0,float px,float py,float pz,float R);
 
 };
 
