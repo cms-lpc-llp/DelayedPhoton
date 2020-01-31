e824b88e743e2c100bd393f02f96776a26690b5a
Thong <thongnguyen1292@gmail.com>
2020-01-30 21:38:17 -0800

    hadd single photon

---
+++ scripts_condor/hadd_delayedphoton_SinglePhoton.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+export X509_USER_PROXY=/storage/user/$(whoami)/my_proxy
+
+OUTDIR=/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/hadd/
+
+if [ ! -d ${OUTDIR} ]
+then
+    echo "${OUTDIR} does not exist. Creating one..."
+    hadoop fs -mkdir ${OUTDIR}
+fi
+
+for sample in \
+SinglePhoton_Run2017B_31Mar2018 \
+SinglePhoton_Run2017D_31Mar2018 \
+SinglePhoton_Run2017F_31Mar2018 \
+SinglePhoton_Run2017C_31Mar2018 \
+SinglePhoton_Run2017E_31Mar2018
+do
+
+    hadd -k -f ${sample}.root /mnt/hadoop/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/jobs/${sample}_Job*.root
+    hadoop fs -put ${sample}.root ${OUTDIR}
+    rm ${sample}.root
+done
+
