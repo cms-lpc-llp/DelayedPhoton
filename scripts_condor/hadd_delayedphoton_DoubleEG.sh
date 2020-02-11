ca510e645891bf72a58915018aba402e5f6ba3a0
Thong <thongnguyen1292@gmail.com>
2020-02-11 13:41:12 -0800

    proper hadd

--- scripts_condor/hadd_delayedphoton_DoubleEG.sh
+++ scripts_condor/hadd_delayedphoton_DoubleEG.sh
@@ -19,7 +19,9 @@ DoubleEG_2017E_31Mar2018
 do
 
     hadd -k -f ${sample}.root /mnt/hadoop/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/jobs/${sample}_Job*.root
+    eval `scram unsetenv -sh`
     gfal-copy -t 2400 -T 2400 -p -f --checksum-mode=both ${sample}.root gsiftp://transfer.ultralight.org/${OUTDIR}/${sample}.root
+	eval `scram runtime -sh`
     rm ${sample}.root
 done
 
