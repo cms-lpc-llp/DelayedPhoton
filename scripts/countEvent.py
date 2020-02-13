9052e6a66280c2aacb458751d9a8b06fd38c5257
Thong <thongnguyen1292@gmail.com>
2020-02-13 14:58:28 -0800

    Adding weight

--- scripts/countEvent.py
+++ scripts/countEvent.py
@@ -14,7 +14,7 @@ cuts['photonSminor'] = " && pho1Sminor < 0.4"
 cuts['photonSmajor'] = " && pho1passSmajorTight" 
 cuts['photonIsolation'] = " && pho1passIsoTight_comboIso "
 cuts['nJets'] = " && n_Jets > 2 "
-cuts['HT'] = " && HT - pho1Pt - pho2Pt > 400" # (n_Photons == 1 && HT - pho1Pt > 400) || (n_Photons >=2 && HT - pho1Pt -pho2Pt > 400)"
+cuts['HT'] = " && (n_Photons == 1 && HT - pho1Pt > 400) || (n_Photons >=2 && HT - pho1Pt -pho2Pt > 400)"
 cuts['Trigger'] = " && HLTDecision[880] == 1"
 
 infile = rt.TFile.Open(argv[1],"r")
@@ -23,11 +23,13 @@ intree = infile.Get("DelayedPhoton")
 total_evt = intree.GetEntries()
 print("All: {} (100%)".format(total_evt))
 selection = ""
+if "GMSB" in infile: 
+    print("Adding weight ")
+    selection = "weight * pileupWeight * triggerEffSFWeight * photonEffSF * "
 for i, cut in enumerate(cuts):
     selection += cuts[cut]
     #print(selection)
-    if i > 11:
-        evtSelected = intree.GetEntries(selection)
-        print("+ {}: {} ({:.2f}%)".format(cut, evtSelected, float(evtSelected)/float(total_evt) * 100.))
+    evtSelected = intree.GetEntries(selection)
+    print("+ {}: {} ({:.2f}%)".format(cut, evtSelected, float(evtSelected)/float(total_evt) * 100.))
 
 
