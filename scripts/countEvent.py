fbe7254c7deaace72d29279893c054bb3346a53d
Thong <thongnguyen1292@gmail.com>
2020-02-13 13:09:45 -0800

    Adding event counter

---
+++ scripts/countEvent.py
@@ -0,0 +1,33 @@
+import ROOT as rt
+from collections import OrderedDict
+from sys import argv
+cuts = OrderedDict()
+cuts['nPhoton>1'] = "n_Photons >= 1 "
+cuts['photonIsEB'] = "&& abs(pho1Eta)<1.4442 "
+cuts['photonPT'] = "&& pho1Pt > 70 "
+cuts['METFilters'] =  " && Flag_HBHENoiseFilter == 1 && Flag_HBHEIsoNoiseFilter ==1 && Flag_goodVertices == 1 && Flag_eeBadScFilter == 1 && Flag_EcalDeadCellTriggerPrimitiveFilter == 1 && Flag_CSCTightHaloFilter == 1  && Flag_badMuonFilter == 1 && Flag_badGlobalMuonFilter == 0 && Flag_duplicateMuonFilter ==0"
+cuts['photonR9'] = "  && pho1R9 > 0.9 "
+cuts['photonTrackVeto'] = " && pho1passTrackVeto "
+cuts['photonHoverE'] = " && pho1passHoverETight"
+cuts['photonIetaeta'] = " && pho1passSigmaIetaIetaTight "
+cuts['photonSminor'] = " && pho1Sminor < 0.4"
+cuts['photonSmajor'] = " && pho1passSmajorTight" 
+cuts['photonIsolation'] = " && pho1passIsoTight_comboIso "
+cuts['nJets'] = " && n_Jets > 2 "
+cuts['HT'] = " && HT - pho1Pt - pho2Pt > 400" # (n_Photons == 1 && HT - pho1Pt > 400) || (n_Photons >=2 && HT - pho1Pt -pho2Pt > 400)"
+cuts['Trigger'] = " && HLTDecision[880] == 1"
+
+infile = rt.TFile.Open(argv[1],"r")
+infile.Print()
+intree = infile.Get("DelayedPhoton")
+total_evt = intree.GetEntries()
+print("All: {} (100%)".format(total_evt))
+selection = ""
+for i, cut in enumerate(cuts):
+    selection += cuts[cut]
+    #print(selection)
+    if i > 11:
+        evtSelected = intree.GetEntries(selection)
+        print("+ {}: {} ({:.2f}%)".format(cut, evtSelected, float(evtSelected)/float(total_evt) * 100.))
+
+
