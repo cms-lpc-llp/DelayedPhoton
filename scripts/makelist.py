b77560f848f07d9bd1dddfd8409039616b83ef76
Thong <thongnguyen1292@gmail.com>
2020-05-08 21:45:34 -0700

    update makelist

--- scripts/makelist.py
+++ scripts/makelist.py
@@ -1,10 +1,10 @@
 import os, sys, glob
 from subprocess import Popen, PIPE
-for signal in glob.glob("/mnt/hadoop/store/group/phys_susy/razor/run2/Run2DelayedPhotonNtuple/MC2016/MC2016CentralGMSB/*"):
-    bare_signal = os.path.basename(signal)#.replace("crab_prod_Run2DelayedPhotonNtupler_Data2017_","")+"_31Mar2018"
+for signal in glob.glob("/mnt/hadoop/store/group/phys_susy/razor/run2/Run2DelayedPhotonNtuple/Data2016/SinglePhoton/*"):
+    bare_signal = os.path.basename(signal).replace("crab_prod_Run2DelayedPhotonNtupler_Data2016_","")
     cmd = "find {} -name \"*.root\"".format(signal)
     #print(cmd)
     filelist, _ = (Popen(cmd, shell=True, stdout=PIPE)).communicate()
-    with open("/storage/user/qnguyen/DelayedPhoton/CMSSW_10_6_6/src/DelayedPhoton/lists/DelayedPhoton2016/MC2016Central/{}.caltech.txt".format(bare_signal),"w") as fileout:
+    with open("/storage/user/qnguyen/DelayedPhoton/CMSSW_10_6_6/src/DelayedPhoton/lists/DelayedPhoton2016/Data2016/{}.caltech.txt".format(bare_signal),"w") as fileout:
         fileout.write(filelist)
         print(bare_signal) 
