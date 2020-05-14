43596bb17b87d099470aefde542e7e7729425edb
Thong <thongnguyen1292@gmail.com>
2020-05-14 02:43:35 -0700

    use correct bkg 17

--- scripts_condor/submit_delayedphoton_bkg.sh
+++ scripts_condor/submit_delayedphoton_bkg.sh
@@ -1,35 +1,45 @@
 #!/bin/sh
 
+export X509_USER_PROXY=/storage/user/$(whoami)/my_proxy
 mkdir -p log
 mkdir -p submit
+
+if [ -z "${CMSSW_BASE}" ]
+then
+    CMSSW_BASE=/storage/user/$(whoami)/DelayedPhoton/CMSSW_10_6_6/
+    echo 'Setting CMSSW_BASE to be \${CMSSW_BASE}'
+fi
 
 cd ../
 RazorAnalyzerDir=`pwd`
 cd -
 
 job_script=${RazorAnalyzerDir}/scripts_condor/runRazorJob_CaltechT2.sh
-filesPerJob=2
+filesPerJob=1
 
 for sample in \
-QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-GJets_HT-40To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
-DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
-
-#DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8
+GJets_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8 \
+GJets_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8 \
+GJets_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8 \
+GJets_HT-40To100_TuneCP5_13TeV-madgraphMLM-pythia8 \
+GJets_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8 \
+DiPhotonJetsBox_M40_80-Sherpa \
+DiPhotonJetsBox_MGG-80toInf_13TeV-Sherpa
+#QCD_HT50to100_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT100to200_TuneCP5_13TeV-madgraphMLM-pythia8
+#QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT200to300_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT300to500_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT500to700_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8 \
+#DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8
+#DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8
 
 do
 	echo "Sample " ${sample}
-	inputfilelist=/src/RazorAnalyzer/lists/Run2/razorNtuplerV4p1/MC_Summer16_reMINIAOD/${sample}.caltech.txt
+	inputfilelist=/src/DelayedPhoton/lists/DelayedPhoton2017/MC/${sample}.txt
 	nfiles=`cat ${CMSSW_BASE}$inputfilelist | wc | awk '{print $1}' `
 	maxjob=`python -c "print int($nfiles.0/$filesPerJob)-1"`
 	analyzer=DelayedPhotonAnalyzer
@@ -42,11 +52,11 @@ do
 		jdl_file=submit/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}.jdl
 		echo "Universe = vanilla" > ${jdl_file}
 		echo "Executable = ${job_script}" >> ${jdl_file}
-		echo "Arguments = ${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob} /store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/orderByPt/jobs/ ${analyzer} ${inputfilelist} no 10 ${filesPerJob} ${jobnumber} ${sample}_Job${jobnumber}_Of_${maxjob}.root" >> ${jdl_file}
+		echo "Arguments = ${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob} /store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/jobs/ ${analyzer} ${inputfilelist} no 10 ${filesPerJob} ${jobnumber} ${sample}_Job${jobnumber}_Of_${maxjob}.root" >> ${jdl_file}
 		echo "Log = log/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}_PC.log" >> ${jdl_file}
 		echo "Output = log/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}_\$(Cluster).\$(Process).out" >> ${jdl_file}
 		echo "Error = log/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}_\$(Cluster).\$(Process).err" >> ${jdl_file}
-		echo 'Requirements=TARGET.OpSysAndVer=="CentOS7"' >> ${jdl_file}
+		#echo 'Requirements=TARGET.OpSysAndVer=="CentOS7"' >> ${jdl_file}
 		echo "should_transfer_files = YES" >> ${jdl_file}
 		echo "RequestMemory = 2000" >> ${jdl_file}
 		echo "RequestCpus = 1" >> ${jdl_file}
@@ -56,6 +66,7 @@ do
                 echo '+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/bbockelm/cms:rhel7"' >> ${jdl_file}
                 echo "+SingularityBindCVMFS = True" >> ${jdl_file}
                 echo "run_as_owner = True" >> ${jdl_file}
+                echo "+JobBatchName = \"${sample}"\" >> ${jdl_file}
 		echo "when_to_transfer_output = ON_EXIT" >> ${jdl_file}
 		echo "Queue 1" >> ${jdl_file}
 		echo "condor_submit submit/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}.jdl"
