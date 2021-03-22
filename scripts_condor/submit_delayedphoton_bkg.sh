29ddf57449a21b803979779946948831933aa9c3
Thong <thongnguyen1292@gmail.com>
2021-03-22 08:22:35 -0700

    no DoubleEG

--- scripts_condor/submit_delayedphoton_bkg.sh
+++ scripts_condor/submit_delayedphoton_bkg.sh
@@ -26,9 +26,6 @@ QCD_Pt-300toInf_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-30to50_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-50to80_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-80to120_EMEnriched_TuneCP5_13TeV_pythia8 \
-QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
-QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
-QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
 GJets_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8 \
 GJets_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8 \
 GJets_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8 \
@@ -36,6 +33,9 @@ GJets_HT-40To100_TuneCP5_13TeV-madgraphMLM-pythia8 \
 GJets_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8 \
 DiPhotonJetsBox_M40_80-Sherpa \
 DiPhotonJetsBox_MGG-80toInf_13TeV-Sherpa \
+#QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+#QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
+#QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
 
 do
 	echo "Sample " ${sample}
