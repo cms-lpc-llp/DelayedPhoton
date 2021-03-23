5fe63506437825f95c0b7221655ca8d5d12a825a
Thong <thongnguyen1292@gmail.com>
2021-03-23 12:10:26 -0700

    update 2018 background samples

--- scripts_condor/hadd_delayedphoton_bkg.sh
+++ scripts_condor/hadd_delayedphoton_bkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export X509_USER_PROXY=/storage/user/$(whoami)/my_proxy
 
-OUTDIR=/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/hadd/
+OUTDIR=/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018/hadd/
 
 if [ ! -d /mnt/hadoop/${OUTDIR} ]
 then
@@ -10,6 +10,11 @@ then
 fi
     
 for sample in \
+    GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+    GJet_Pt-20toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
+    GJet_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+    DiPhotonJetsBox_M40_80-Sherpa \
+    DiPhotonJetsBox_MGG-80toInf_13TeV-Sherpa \
     QCD_Pt-120to170_EMEnriched_TuneCP5_13TeV_pythia8 \
     QCD_Pt-15to20_EMEnriched_TuneCP5_13TeV_pythia8 \
     QCD_Pt-170to300_EMEnriched_TuneCP5_13TeV_pythia8 \
@@ -18,18 +23,11 @@ for sample in \
     QCD_Pt-30to50_EMEnriched_TuneCP5_13TeV_pythia8 \
     QCD_Pt-50to80_EMEnriched_TuneCP5_13TeV_pythia8 \
     QCD_Pt-80to120_EMEnriched_TuneCP5_13TeV_pythia8 \
-    QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
-    QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
-    QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
-    GJets_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8 \
-    GJets_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8 \
-    GJets_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8 \
-    GJets_HT-40To100_TuneCP5_13TeV-madgraphMLM-pythia8 \
-    GJets_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8 \
-    DiPhotonJetsBox_M40_80-Sherpa \
-    DiPhotonJetsBox_MGG-80toInf_13TeV-Sherpa
+#    QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+#    QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
+#    QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
 do
-    hadd -k -f ${sample}.root /mnt/hadoop/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/jobs/${sample}_Job*.root
+    hadd -k -f ${sample}.root /mnt/hadoop/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018/jobs/${sample}_Job*.root
     eval `scram unsetenv -sh`
     gfal-copy -t 2400 -T 2400 -p -f --checksum-mode=both ${sample}.root gsiftp://transfer.ultralight.org/${OUTDIR}/${sample}.root
 	eval `scram runtime -sh`
--- scripts_condor/submit_delayedphoton_bkg.sh
+++ scripts_condor/submit_delayedphoton_bkg.sh
@@ -6,7 +6,7 @@ mkdir -p submit
 
 if [ -z "${CMSSW_BASE}" ]
 then
-    CMSSW_BASE=/storage/user/$(whoami)/DelayedPhoton/CMSSW_10_6_6/
+    CMSSW_BASE=/storage/user/$(whoami)/DelayedPhoton/CMSSW_10_6_12/
     echo 'Setting CMSSW_BASE to be \${CMSSW_BASE}'
 fi
 
@@ -18,6 +18,11 @@ job_script=${RazorAnalyzerDir}/scripts_condor/runRazorJob_CaltechT2.sh
 filesPerJob=1
 
 for sample in \
+GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+GJet_Pt-20toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
+GJet_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+DiPhotonJetsBox_M40_80-Sherpa \
+DiPhotonJetsBox_MGG-80toInf_13TeV-Sherpa \
 QCD_Pt-120to170_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-15to20_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-170to300_EMEnriched_TuneCP5_13TeV_pythia8 \
@@ -26,20 +31,13 @@ QCD_Pt-300toInf_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-30to50_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-50to80_EMEnriched_TuneCP5_13TeV_pythia8 \
 QCD_Pt-80to120_EMEnriched_TuneCP5_13TeV_pythia8 \
-QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
-QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
-QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8
-#GJets_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8 \
-#GJets_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8 \
-#GJets_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8 \
-#GJets_HT-40To100_TuneCP5_13TeV-madgraphMLM-pythia8 \
-#GJets_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8 \
-#DiPhotonJetsBox_M40_80-Sherpa \
-#DiPhotonJetsBox_MGG-80toInf_13TeV-Sherpa
+#QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8 \
+#QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8 \
+#QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8
 
 do
 	echo "Sample " ${sample}
-	inputfilelist=/src/DelayedPhoton/lists/DelayedPhoton2017/MC/${sample}.txt
+	inputfilelist=/src/DelayedPhoton/lists/DelayedPhoton2018/MC/${sample}.txt
 	nfiles=`cat ${CMSSW_BASE}$inputfilelist | wc | awk '{print $1}' `
 	maxjob=`python -c "print int($nfiles.0/$filesPerJob)-1"`
 	analyzer=DelayedPhotonAnalyzer
@@ -52,7 +50,7 @@ do
 		jdl_file=submit/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}.jdl
 		echo "Universe = vanilla" > ${jdl_file}
 		echo "Executable = ${job_script}" >> ${jdl_file}
-		echo "Arguments = ${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob} /store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2017/jobs/ ${analyzer} ${inputfilelist} no 10 ${filesPerJob} ${jobnumber} ${sample}_Job${jobnumber}_Of_${maxjob}.root" >> ${jdl_file}
+		echo "Arguments = ${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob} /store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018/jobs/ ${analyzer} ${inputfilelist} no 10 ${filesPerJob} ${jobnumber} ${sample}_Job${jobnumber}_Of_${maxjob}.root" >> ${jdl_file}
 		echo "Log = log/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}_PC.log" >> ${jdl_file}
 		echo "Output = log/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}_\$(Cluster).\$(Process).out" >> ${jdl_file}
 		echo "Error = log/${analyzer}_${sample}_Job${jobnumber}_Of_${maxjob}_\$(Cluster).\$(Process).err" >> ${jdl_file}
