4d35ebc5710970b52ba7c262ba026980b0cb261b
YeongDeok Seo <yeseo@cern.ch>
2021-04-05 20:18:15 -0700

    update N and C for ABC pre and ultra legacy

--- analyzers/ZeeTiming.h
+++ analyzers/ZeeTiming.h
@@ -12,10 +12,17 @@ class ZeeTiming: public RazorAnalyzer {
 		float getPedestalNoise(TTree *tree, vector <uint> & start_time, vector <uint> & end_time, uint time, uint detID);
 		float getADCToGeV( uint run, int isFromEB);
 
-        const double N_EB = 30.4;   //ns
+		// for 2018ABC pre-legacy
+        const double N_EB = 34.1;   //ns
         const double C_EB = 0.095; //ns
-        const double N_EB_MC = 34.3; //ns
-        const double C_EB_MC = 0.103; //ns
+        const double N_EB_MC = 48.3; //ns
+        const double C_EB_MC = 0.095; //ns
+
+		// for 2018ABC ultra-legacy
+        const double N_EB = 33.5;   //ns
+        const double C_EB = 0.096; //ns
+        const double N_EB_MC = 49.0; //ns
+        const double C_EB_MC = 0.097; //ns
 
         uint start_run_tmp;
         uint end_run_tmp;	
