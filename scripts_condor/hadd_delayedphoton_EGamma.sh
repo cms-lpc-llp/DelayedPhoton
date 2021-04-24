9a27ca9c653aaecc77507c80624a81e9421fa94e
Thong <thongnguyen1292@gmail.com>
2021-04-23 19:16:17 -0700

    use parhadd for very big data

--- scripts_condor/hadd_delayedphoton_EGamma.sh
+++ scripts_condor/hadd_delayedphoton_EGamma.sh
@@ -2,7 +2,7 @@
 
 export X509_USER_PROXY=/storage/user/$(whoami)/my_proxy
 
-OUTDIR=/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018/hadd/
+OUTDIR=/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018_pho_corr/hadd/
 
 if [ ! -d ${OUTDIR} ]
 then
@@ -14,11 +14,11 @@ for sample in \
     EGamma_Run2018A \
     EGamma_Run2018B \
     EGamma_Run2018C \
-    EGamma_Run2018D_v2 \
+    EGamma_Run2018D \
 
 do
 
-    hadd -k -f ${sample}.root /mnt/hadoop/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018/jobs/${sample}_Job*.root
+    python /storage/user/qnguyen/DelayedPhoton/CMSSW_10_6_12/src/DelayedPhoton/scripts_condor/ParHadd.py ${sample}.root "/mnt/hadoop/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2018_pho_corr/jobs/${sample}_Job*.root"
     eval `scram unsetenv -sh`
     gfal-copy -t 2400 -T 2400 -p -f --checksum-mode=both ${sample}.root gsiftp://transfer.ultralight.org/${OUTDIR}/${sample}.root
 	eval `scram runtime -sh`
