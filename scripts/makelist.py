9ba41bfb98ecc349a436766a907a82a6af74e7cc
Thong <thongnguyen1292@gmail.com>
2021-05-30 17:18:26 -0700

    new signal sample

--- scripts/makelist.py
+++ scripts/makelist.py
@@ -1,10 +1,10 @@
 import os, sys, glob
 from subprocess import Popen, PIPE
-for signal in glob.glob("/mnt/hadoop/store/group/phys_susy/razor/run2/Run2DelayedPhotonNtuple/MC2018/DYJetsToLL/*"):
+for signal in glob.glob("/mnt/hadoop/store/group/phys_susy/razor/run2/Run2DelayedPhotonNtuple/MC2018/MC2018CentralGMSB/*"):
     bare_signal = os.path.basename(signal)#.replace("crab_prod_Run2DelayedPhotonNtupler_Data2018_","")
     cmd = "find {} -name \"*.root\"".format(signal)
     #print(cmd)
     filelist, _ = (Popen(cmd, shell=True, stdout=PIPE)).communicate()
-    with open("/storage/user/qnguyen/DelayedPhoton/CMSSW_10_6_12/src/DelayedPhoton/lists/DelayedPhoton2018/MC/{}.txt".format(bare_signal),"w") as fileout:
+    with open("/storage/user/qnguyen/DelayedPhoton/CMSSW_10_6_12/src/DelayedPhoton/lists/DelayedPhoton2018/Signal/{}.txt".format(bare_signal),"w") as fileout:
         fileout.write(filelist)
         print(bare_signal) 
