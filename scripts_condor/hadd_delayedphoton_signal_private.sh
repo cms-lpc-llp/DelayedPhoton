63ed92f956dd25f873b7aa0fae47745f336f98f0
Thong <thongnguyen1292@gmail.com>
2021-08-08 21:59:16 -0700

    hadd 2016

--- scripts_condor/hadd_delayedphoton_signal_private.sh
+++ scripts_condor/hadd_delayedphoton_signal_private.sh
@@ -2,14 +2,12 @@
 export X509_USER_PROXY=/storage/af/user/$(whoami)/my_proxy
 x509loc=${X509_USER_PROXY}
 
-OUTDIR=/storage/cms/store/group/phys_llp/DelayedPhoton/2016/hadd/
+OUTDIR=/storage/af/user/qnguyen/DelayedPhoton/CMSSW_10_6_12/src/DelayedPhoton/local_storage///store/group/phys_llp/DelayedPhoton/2016/hadd/
 
 if [ ! -d ${OUTDIR} ]
 then
     echo "${OUTDIR} does not exist. Creating one..."
-    eval `scram unsetenv -sh`
-    gfal-mkdir gsiftp://transfer-lb.ultralight.org/${OUTDIR}
-	eval `scram runtime -sh`
+    mkdir -p ${OUTDIR}
 fi
     
 for sample in \
@@ -20,25 +18,18 @@ GMSB_L250TeV_Ctau50cm_13TeV-pythia8 \
 GMSB_L300TeV_Ctau50cm_13TeV-pythia8 \
 GMSB_L350TeV_Ctau50cm_13TeV-pythia8 \
 GMSB_L400TeV_Ctau50cm_13TeV-pythia8 \
-GMSB_L100TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L100TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L100TeV_Ctau1cm_13TeV-pythia8 \
-GMSB_L150TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L150TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L150TeV_Ctau1cm_13TeV-pythia8 \
-GMSB_L200TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L200TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L200TeV_Ctau1cm_13TeV-pythia8 \
-GMSB_L250TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L250TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L250TeV_Ctau1cm_13TeV-pythia8 \
-GMSB_L300TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L300TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L300TeV_Ctau1cm_13TeV-pythia8 \
-GMSB_L350TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L350TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L350TeV_Ctau1cm_13TeV-pythia8 \
-GMSB_L400TeV_Ctau10000cm_13TeV-pythia8 \
 GMSB_L400TeV_Ctau100cm_13TeV-pythia8 \
 GMSB_L400TeV_Ctau1cm_13TeV-pythia8 \
 
@@ -46,7 +37,7 @@ do
     correctSample=${sample/L/L-}
     correctSample=${correctSample/Ctau/Ctau-}
     echo "${correctSample}"
-    hadd -k -f ${correctSample}.root /storage/cms/store/group/phys_llp/DelayedPhoton/2016/jobs/${sample}_Job*.root
-    env -i X509_USER_PROXY=${x509loc} gfal-copy -t 2400 -T 2400 -p -f --checksum-mode=both ${correctSample}.root gsiftp://transfer-lb.ultralight.org/${OUTDIR}
+    hadd -k -f ${correctSample}.root /storage/af/user/qnguyen/DelayedPhoton/CMSSW_10_6_12/src/DelayedPhoton/local_storage///store/group/phys_llp/DelayedPhoton/2016/jobs/${sample}_Job*.root
+    cp ${correctSample}.root ${OUTDIR}/${correctSample}.root
     rm ${correctSample}.root
 done
